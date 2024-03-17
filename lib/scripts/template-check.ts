393a1e970caab6b7d203e8df6f350c9647344461
Shubhom Srivastava <shbhomsrivastav@gmail.com>
2024-03-17 13:07:20 +0530

    changes in template-check

--- lib/scripts/template-check.ts
+++ lib/scripts/template-check.ts
@@ -1,40 +1,41 @@
 import MarkdownIt from "markdown-it";
 
 const parser = new MarkdownIt();
+export function parseBody(issueBody: string) {
+  const parsedContent = parser.render(issueBody);
 
-const issueBody = process.argv[2];
+  const requiredSections = [
+    "What",
+    "Short Description",
+    "Expected Behavior",
+    "Seeing",
+    "Environment",
+  ];
 
-const parsedContent = parser.render(issueBody);
+  let errorMessage = "";
 
-const requiredSections = [
-  "What",
-  "Short Description",
-  "Expected Behavior",
-  "Seeing",
-  "Environment",
-];
+  requiredSections.forEach((section) => {
+    const sectionStart = parsedContent.indexOf(
+      `<h4 id="${section}">${section}</h4>`,
+    );
+    const sectionEnd = parsedContent.indexOf('<h4 id="', sectionStart + 1);
 
-let errorMessage = "";
+    if (
+      sectionStart === -1 ||
+      sectionEnd === -1 ||
+      parsedContent.substring(sectionStart, sectionEnd).trim() === ""
+    ) {
+      errorMessage += ` - Missing or empty "${section}" section.\n`;
+    }
+  });
 
-requiredSections.forEach((section) => {
-  const sectionStart = parsedContent.indexOf(
-    `<h4 id="${section}">${section}</h4>`,
-  );
-  const sectionEnd = parsedContent.indexOf('<h4 id="', sectionStart + 1);
-
-  if (
-    sectionStart === -1 ||
-    sectionEnd === -1 ||
-    parsedContent.substring(sectionStart, sectionEnd).trim() === ""
-  ) {
-    errorMessage += ` - Missing or empty "${section}" section.\n`;
+  if (errorMessage) {
+    console.error("Issue template format is invalid:\n" + errorMessage);
+    process.exit(1); // Exit with a failure status
+  } else {
+    console.log("Issue template format is valid!");
+    process.exit(0); // Exit with a success status
   }
-});
-
-if (errorMessage) {
-  console.error("Issue template format is invalid:\n" + errorMessage);
-  process.exit(1); // Exit with a failure status
-} else {
-  console.log("Issue template format is valid!");
-  process.exit(0); // Exit with a success status
 }
+
+// const issueBody = process.argv[2];
